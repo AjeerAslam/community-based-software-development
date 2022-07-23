73f586c71153a4b1efb0a6920ae7cf5dfce865f2
ajeeraslam@gmail.com <ajeeraslam@gmail.com>
2022-07-23 18:49:15 +0530

    after adding login file

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.index),
+    path('',views.index,name='home'),
     path('accept/', views.accept ),
 
 ]
--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from curses.ascii import HT
 from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from home.models import Modules
 from home.models import Clients
 from django.http import JsonResponse
 from home.models import Projects
@@ -15,10 +16,10 @@ def index(request):
     projects_task=Projects.objects.raw('SELECT * FROM projects WHERE pr_status="waiting"')
     projects_review=Projects.objects.raw('SELECT * FROM projects WHERE pr_status="review"')
     projects_completed=Projects.objects.raw('SELECT * FROM projects WHERE pr_status="completed"')
-    projects_completed=Projects.objects.raw('SELECT * FROM projects WHERE pr_status="completed"')
-    projects_completed=Projects.objects.raw('SELECT * FROM projects WHERE pr_status="completed"')
-    projects_completed=Projects.objects.raw('SELECT * FROM projects WHERE pr_status="completed"')
-    return render(request,'index.html',{'Projects':projects_latest,'Projects_task':projects_task,'Projects_review':projects_review,'Projects_completed':projects_completed})
+    modules=Modules.objects.raw('SELECT * FROM modules WHERE md_status="new"')
+    modules_review=Modules.objects.raw('SELECT * FROM modules WHERE md_status="review"')
+    modules_completed=Modules.objects.raw('SELECT * FROM modules WHERE md_status="completed"')
+    return render(request,'index.html',{'Projects':projects_latest,'Projects_task':projects_task,'Projects_review':projects_review,'Projects_completed':projects_completed,'Modules':modules,'Modules_review':modules_review,'Modules_completed':modules_completed})
 
 def accept(request):
     print(request)
